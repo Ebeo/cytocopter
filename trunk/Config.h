53a4475272a0e5f5ccb72f7aefc6ab9aceddb6cb
daniel@phi-numbers.de <daniel@phi-numbers.de@4dea2199-f6c1-a993-bef8-1eb2437f59ee>
2011-09-13 22:40:52 +0000

    switching back to arduino

---
+++ trunk/Config.h
@@ -0,0 +1,21 @@
+/**
+ *	Configuration
+ */
+#define MINTHROTTLE		1150
+#define	MAXTHROTTLE		2000
+#define MOTORPINS		9, 10, 11, 3
+
+
+#define YAW_DIRECTION	1
+
+
+
+
+
+/**
+ *	Definitions
+ */
+#define ROLL       0
+#define PITCH      1
+#define YAW        2
+#define THROTTLE   3
