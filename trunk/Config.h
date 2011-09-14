705897d8b523ab8ea450165004fd0356884bbca3
daniel@phi-numbers.de <daniel@phi-numbers.de@4dea2199-f6c1-a993-bef8-1eb2437f59ee>
2011-09-14 10:45:10 +0000

    added more

--- trunk/Config.h
+++ trunk/Config.h
@@ -2,12 +2,14 @@
  *	Configuration
  */
 #define MINTHROTTLE		1150
-#define	MAXTHROTTLE		2000
-#define MOTORPINS		9, 10, 11, 3
+#define	MAXTHROTTLE	  2000
+#define MOTORPINS		  9, 10, 11, 3
 
 
 #define YAW_DIRECTION	1
 
+
+#define MIDRC          1500
 
 
 
@@ -15,7 +17,17 @@
 /**
  *	Definitions
  */
-#define ROLL       0
-#define PITCH      1
-#define YAW        2
-#define THROTTLE   3
+#define ROLL       		0
+#define PITCH      		1
+#define YAW        		2
+#define THROTTLE   		3
+
+//RX PIN assignment inside the port //for PORTD
+#define THROTTLEPIN		2
+#define ROLLPIN			  4
+#define PITCHPIN		   5
+#define YAWPIN			   6
+#define AUX1PIN			  7
+#define AUX2PIN			  7   //unused just for compatibility with MEGA
+#define CAM1PIN			  7   //unused just for compatibility with MEGA
+#define CAM2PIN			  7   //unused just for compatibility with MEGA
