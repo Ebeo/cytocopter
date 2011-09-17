4890e946c1769749423d4874f88c0fb359f66aa4
daniel <daniel@phi-numbers.de>
2011-09-17 15:48:42 +0000

    oop

--- Config.h
+++ Config.h
@@ -1,15 +1,15 @@
 /**
  *	Configuration
  */
-#define MINTHROTTLE		1150			 // minimal throttle
+#define MINTHROTTLE		1150			// minimal throttle
 #define	MAXTHROTTLE		2000			// maximal throttle
-#define MOTORPINS		9, 10, 11, 3  // motor pins
+#define MOTORPINS		9, 10, 11, 3	// motor pins
 
-#define MINCOMMAND		1000			  // minimal rc throttle command
-#define MAXCOMMAND		2000			  // maximal rc throttle command
-#define MIDRC 1500						    // rc middle position
+#define MINCOMMAND		1000			// minimal rc throttle command
+#define MAXCOMMAND		2000			// maximal rc throttle command
+#define MIDRC 1500						// rc middle position
 
-#define YAW_DIRECTION	1				  // yaw inverted?
+#define YAW_DIRECTION	1				// yaw inverted?
 
 
 /**
@@ -22,10 +22,10 @@
 
 //RX PIN assignment inside the port //for PORTD
 #define THROTTLEPIN				2
-#define ROLLPIN					  4
-#define PITCHPIN				   5
-#define YAWPIN					   6
-#define AUX1PIN					  7
-#define AUX2PIN					  7   // unused just for compatibility with MEGA
-#define CAM1PIN				  	7   // unused just for compatibility with MEGA
-#define CAM2PIN					  7   // unused just for compatibility with MEGA
+#define ROLLPIN					4
+#define PITCHPIN				5
+#define YAWPIN					6
+#define AUX1PIN					7
+#define AUX2PIN					7   //unused just for compatibility with MEGA
+#define CAM1PIN					7   //unused just for compatibility with MEGA
+#define CAM2PIN					7   //unused just for compatibility with MEGA
