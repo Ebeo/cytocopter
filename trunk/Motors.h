0b139265cfebf80b5634a689359e77de74093576
daniel@phi-numbers.de <daniel@phi-numbers.de@4dea2199-f6c1-a993-bef8-1eb2437f59ee>
2011-09-17 15:47:06 +0000

    oop

---
+++ trunk/Motors.h
@@ -0,0 +1,22 @@
+#ifndef Motors_H
+#define Motors_H
+
+#include "WProgram.h"
+
+class Motors
+{
+private:
+  int16_t motor[4];
+  uint8_t PWM_PINS[4];  
+  void contrainMotorSpeed(void);
+
+public:
+  Motors();
+  void setMotorSpeed(int16_t speed, uint8_t num);
+  void writeMotors(void);
+  void initializeMotors(void);
+};
+
+#endif
+
+
